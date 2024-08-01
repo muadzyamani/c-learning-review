53610d866d06540df451d9cb860a052468e2cc92
Muadz <muadzyamani@gmail.com>
2024-08-01 12:08:31 +0800

    Add random-numbers.c

---
+++ random-numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  // pseudo random numbers = A set of values or numbers that are statistically random
+  //                         (Don't use these for any sort of cryptographic security)
+
+  srand(time(0));
+
+  int number1 = (rand() % 6) + 1; // Generate a random number from 1 - 6
+  int number2 = (rand() % 6) + 1;
+  int number3 = (rand() % 6) + 1;
+
+  printf("%d\n", number1);
+  printf("%d\n", number2);
+  printf("%d\n", number3);
+
+
+  return 0;
+}
