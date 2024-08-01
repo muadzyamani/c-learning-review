9f809f9eed3074e5d34f1a4db6bfb4efad8634b8
Muadz <muadzyamani@gmail.com>
2024-08-01 15:52:10 +0800

    Add pointers

---
+++ pointers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void printAge(int *pAge)
+{
+  printf("You are %d years old", *pAge); //de-referencing
+}
+
+int main()
+{
+  // pointer = a 'variable-like" reference that holds a memory address to another variable, array, etc.
+  //           some tasks are performed more easily with pointers
+  //           * = indirection operator (value at address)
+
+  int age = 21;
+  int *pAge = NULL; // good practice to assign NULL if declaring a pointer
+  pAge = &age;
+
+  printf("address of age: %p\n", &age);
+  printf("value of pAge: %p\n", pAge);
+
+  printf("size of age: %d bytes\n", sizeof(age));
+  printf("size of pAge: %d bytes\n", sizeof(pAge));
+
+  printf("value of age: %d\n", age);
+  printf("value at stored address: %d\n", *pAge); //de-referencing
+
+  printAge(pAge);
+
+  return 0;
+}
